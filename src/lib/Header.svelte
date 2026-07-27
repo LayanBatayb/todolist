9b5b9a6418d4138169cb3d5b4b5bfa2c62a427ad
Layan <layan6@gmail.com>
2026-07-27 07:35:35 +0300

    header file

---
+++ src/lib/Header.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import dayjs from "dayjs";
+  import "dayjs/locale/ar";
+  dayjs.locale("ar");
+
+  let Period = dayjs().format("a") == "م" ? "مساء" : "صباح";
+</script>
+
+ <header class="flex justify-between">
+    <div>
+      <h1 class="text-4xl text-pink-900 mb-2">{Period} الخير 👋</h1>
+      <h2 class="text-2xl text-pink-900/50">
+        {dayjs().format("اليوم dddd  D MMM YYYY ")}
+      </h2>
+    </div>
+
+    <div class="bg-pink-900/40 w-28 h-14"></div>
+  </header>
